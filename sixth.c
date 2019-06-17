7082e8f4b4e299218aa92c19a826e69981d5ff14
Neeharika-387 <51849507+Neeharika-387@users.noreply.github.com>
2019-06-17 18:26:31 +0530

    Create sixth.c

---
+++ sixth.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n;
+ scanf("%d",&n);
+ if((n%4)==0)
+ {
+   printf("LEAP YEAR");
+ }
+ else
+ {
+    printf("NON LEAP YEAR");
+ }
+ getch();;
+ return 0;
+ }
