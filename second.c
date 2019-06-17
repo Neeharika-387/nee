c157bb83385f3cb6680e5c4a89e80405dd7537e8
Neeharika-387 <51849507+Neeharika-387@users.noreply.github.com>
2019-06-17 17:33:15 +0530

    Update second.c

--- second.c
+++ second.c
@@ -4,10 +4,12 @@ void main()
 {
 int n;
 scanf("%d",&n);
-if(n%2==0)
-printf("EVEN");
-else
-printf("ODD");
+  if(n%2==0)
+    printf("EVEN");
+  else if(n%2==1)
+    printf("ODD");
+  else
+    printf("invalid");
 getch();
 return 0;
 }
