d9f0776372a3eec0b6ef90f6b057ce7ff57867d4
Neeharika-387 <51849507+Neeharika-387@users.noreply.github.com>
2019-06-17 18:08:11 +0530

    Create fifth.c

---
+++ fifth.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("%d",a);
+else if((b>a)&&(b>c))
+printf("%d",b);
+else
+printf("%d",c);
+getch();
+return 0;
+}
